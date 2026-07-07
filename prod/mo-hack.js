7021430af333a54d103eb23a62d4acbf5c42462c
Hans Åge Martinsen <hamartin@moshwire.com>
2026-07-08 01:02:06 +0200

    Added property information to the jsdoc.

--- prod/mo-hack.js
+++ prod/mo-hack.js
@@ -2,9 +2,12 @@
  * To run the hack script an un-named argument with the host name of the host
  * you want to attack must be given.
  * 
- * @param {NS} ns
+ * @param {NS} ns                 - Netscript context
+ * @property {string} ns.args[0]  - Required host name to hack
+ * @example ns.run(<PATH TO SCRIPT>, <NUMBER OF THREADS>, <TARGET HOST>);
+ * @example run <PATH TO SCRIPT> <TARGET HOST> -t <NUMBER OF THREADS>
  */
-export async function main(ns) {
+export async function main(ns, arg0) {
     if (ns.args[0] === undefined) ns.alert("No host name argument was given.");
     const targetHost = String(ns.args[0]);
     await ns.hack(targetHost);
--- prod/mo-weaken.js
+++ prod/mo-weaken.js
@@ -2,7 +2,10 @@
  * To run the weaken script an un-named argument with the host name of the host
  * you want to attack must be given.
  * 
- * @param {NS} ns
+ * @param {NS} ns                - Netscript context
+ * @property {string} ns.args[0] - Required host name to weaken
+ * @example ns.run(<PATH TO SCRIPT>, <NUMBER OF THREADS>, <TARGET HOST>);
+ * @example run <PATH TO SCRIPT> <TARGET HOST> -t <NUMBER OF THREADS>
  */
 export async function main(ns) {
     if (ns.args[0] === undefined) ns.alert("No host name argument was given.");
